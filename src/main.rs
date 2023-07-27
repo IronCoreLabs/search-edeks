51a3f24f5fd0d5a0634fe7646b7b4d4810ad6cdb
Murph Murphy <murph@clurictec.com>
2023-07-27 16:07:58 -0600

    Add mismatched filter option

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
+mod filters;
 mod util;
 mod proto {
     include!(concat!(env!("OUT_DIR"), "/proto/mod.rs"));
 }
 
 use base64::{engine::general_purpose::STANDARD, Engine};
+use clap::{arg, command, value_parser, ArgAction, ArgGroup};
+use filters::{execute_filter, Filter};
 use std::{
+    convert::identity,
     fs::File,
     io::{BufRead, BufReader},
     path::{Path, PathBuf},
 };
 use util::{edek_from_bytes, write_file, EdekFileEntry};
-
-use clap::{arg, command, value_parser, ArgAction, ArgGroup};
 
 fn main() {
     let matches = command!()
@@ -19,8 +21,16 @@ fn main() {
             arg!(
                 -i --id <VALUE> "Sets the KMS config ID we're searching for"
             )
-            .required(true)
             .value_parser(value_parser!(i32)),
+        )
+        .arg(arg!(
+            -m --mismatched "Searches for mismatches between the KMS config ID in the EDEK header and the leased key used to encrypt the EDEK. Resulting EDEKs must be rekeyed with TSP 4.11.1+ to repair."
+        ).action(ArgAction::SetTrue))
+        .group(
+            ArgGroup::new("search")
+                .required(true)
+                .args(&["id", "mismatched"])
+                .multiple(true),
         )
         .arg(
             arg!(
@@ -43,8 +53,15 @@ fn main() {
         .arg(arg!(-v --verbose "Output identifier and original EDEK (and error message if applicable). If not enabled, only identifiers will be output").action(ArgAction::SetTrue))
         .get_matches();
 
-    // get our required arguments
-    let config_id = matches.get_one::<i32>("id").expect("id is required");
+    // filter args
+    let config_id_option = matches.get_one::<i32>("id").map(|c| Filter::ConfigId(*c));
+    let mismatched = if matches.get_flag("mismatched") {
+        Some(Filter::Mismatched)
+    } else {
+        None
+    };
+
+    // io args
     let edek_file_path = matches
         .get_one::<PathBuf>("file")
         .expect("file is required");
@@ -53,35 +70,41 @@ fn main() {
     let debug: bool = matches.get_flag("debug");
     let verbose: bool = matches.get_flag("verbose");
 
-    // read the edek tuples line by line
+    // open the edek tuples file handle, later to be read as a buffered streaming iterator
     let edek_file = File::open(edek_file_path)
-        .map_err(|e| format!("Failed to open EDEK file: {}", e))
+        .map_err(|e| format!("Failed to open EDEK file: {e}"))
         .unwrap();
     // zip an index in so we can give line numbers
     let edek_lines = BufReader::new(edek_file).lines().zip(1..);
+    // if we ever hit memory issues while writing the whole vec out of memory at once, we could write directly to shared write handlers
     let mut found_lines: Vec<EdekFileEntry> = vec![];
     let mut found_broken: Vec<(String, String, String)> = vec![];
+    // a list of filters to attempt to match
+    let active_filters: Vec<Filter> = vec![config_id_option, mismatched]
+        .into_iter()
+        .filter_map(identity)
+        .collect();
     for line in edek_lines {
         if let (Ok(edek_entry), line_number) = line {
             if debug {
-                println!("edek string: {}", edek_entry);
+                println!("edek string: {edek_entry}");
             };
             let (identifier, edek) = ron::from_str::<EdekFileEntry>(&edek_entry)
-                .map_err(|e| format!("Unexpected error processing line {}: {}", line_number, e))
+                .map_err(|e| format!("Unexpected error processing line {line_number}: {e}"))
                 .unwrap();
             // decode the edek string in the desired format
             // if we fail, log it, but keep going in case there are lines that match
             let decode_attempt = if use_base64 {
                 STANDARD
                     .decode(edek.clone())
-                    .map_err(|e| format!("EDEK was not base64: {}", e))
+                    .map_err(|e| format!("EDEK was not base64: {e}"))
             } else if use_hex {
                 let stripped = if edek.starts_with("0x") || edek.starts_with("0X") {
                     edek.chars().skip(2).collect()
                 } else {
                     edek.clone()
                 };
-                hex::decode(stripped).map_err(|e| format!("EDEK was not hex: {}", e))
+                hex::decode(stripped).map_err(|e| format!("EDEK was not hex: {e}"))
             } else {
                 // this should've already been handled by clap, but writing again so Rust is happy
                 panic!("Base64 or Hex format must be specified.");
@@ -94,35 +117,56 @@ fn main() {
                     match parse_attempt {
                         Ok(parsed_edek) => {
                             if debug {
-                                println!("parsed proto: {}, line {}", parsed_edek, line_number);
+                                println!("parsed proto: {parsed_edek}, line {line_number}");
                             }
-                            // do the actual comparison we care about
-                            if parsed_edek.kmsConfigId == *config_id {
-                                found_lines.push((identifier, edek));
+                            let (matched_filters, failures): (Vec<_>, Vec<_>) = active_filters
+                                .iter()
+                                .map(|filter| execute_filter(filter, &parsed_edek))
+                                .partition(Result::is_ok);
+
+                            if !failures.is_empty() {
+                                let message = failures.into_iter().map(Result::unwrap_err).fold(
+                                    format!("Failure on line {line_number}: "),
+                                    |acc, failure| format!("{} {}", acc, failure),
+                                );
+                                if debug {
+                                    println!("WARNING: {message}");
+                                }
+                                // there was at least one active filter that failed in some way, add this line to the broken ones
+                                found_broken.push((identifier, edek, message))
+                            } else {
+                                if matched_filters
+                                    .into_iter()
+                                    .map(Result::unwrap)
+                                    .all(identity)
+                                {
+                                    // the current line passed all filters, add it to found
+                                    found_lines.push((identifier, edek));
+                                }
                             }
                         }
                         Err(e) => {
-                            println!(
-                                "WARNING: Encountered an unparsable EDEK on line {}: {}",
-                                line_number, e
-                            );
+                            if debug {
+                                println!(
+                                    "WARNING: Encountered an unparsable EDEK on line {line_number}: {e}"
+                                );
+                            }
                             found_broken.push((
                                 identifier,
                                 edek,
-                                format!("Encountered an unparsable EDEK: {}", e),
+                                format!("Encountered an unparsable EDEK: {e}"),
                             ));
                         }
                     }
                 }
                 Err(e) => {
                     println!(
-                        "Encountered an incorrectly formatted EDEK at line {}: {}",
-                        line_number, e
+                        "Encountered an incorrectly formatted EDEK at line {line_number}: {e}"
                     );
                     found_broken.push((
                         identifier,
                         edek,
-                        format!("Encountered an incorrectly formated EDEK: {}", e),
+                        format!("Encountered an incorrectly formated EDEK: {e}"),
                     ));
                 }
             }
@@ -139,7 +183,7 @@ fn main() {
             path.display()
         );
     } else {
-        println!("Found no EDEKs with the given config ID.");
+        println!("Found no EDEKs matching the search parameters.");
     }
     if !found_broken.is_empty() {
         let path = Path::new("broken-edeks.txt").to_path_buf();
